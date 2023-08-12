d8378ed944318c5c51b4a3caa92fa81603239d66
Wojuade Abdul Afeez <wojucrown7@gmail.com>
2023-08-12 21:02:11 +0100

    complete meme maker

---
+++ MemeMaker/CaptionOption.swift
@@ -0,0 +1,14 @@
+//
+//  CaptionOption.swift
+//  MemeMaker
+//
+//  Created by Wojuade Abdul Afeez on 11/08/2023.
+//
+
+import Foundation
+
+
+struct CaptionOptions {
+    let emoji : String
+    let caption : String
+}
--- MemeMaker/ViewController.swift
+++ MemeMaker/ViewController.swift
@@ -9,11 +9,60 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var topSegmentedControl: UISegmentedControl!
+    @IBOutlet weak var bottomSegmentedControl: UISegmentedControl!
+    
+    @IBOutlet weak var topCaptionLabel: UILabel!
+    
+    @IBOutlet weak var bottomCaptionLabel: UILabel!
+    let topChoices : [CaptionOptions] = [
+    CaptionOptions(emoji: "😎", caption: "You know what is cool?"),
+    CaptionOptions(emoji: "😡", caption: "You know what makes me so mad?"),
+    CaptionOptions(emoji: "❤️", caption: "You know what i love?"),
+    ]
+    let bottomChoice : [CaptionOptions] = [
+        CaptionOptions(emoji: "🐱", caption: "cats learning how to code"),
+        CaptionOptions(emoji: "🐕", caption: "dogs carrying logs"),
+        CaptionOptions(emoji: "🐒", caption: "Monkey being funky"),
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+       loadSegmentedControl(with: topChoices, segmentedControl: topSegmentedControl)
+        loadSegmentedControl(with: bottomChoice, segmentedControl: bottomSegmentedControl)
+        updateCaption()
+       
     }
 
 
+    @IBAction func segmentedControlAction(_ sender: UISegmentedControl) {
+        updateCaption()
+    }
+    
+    func loadSegmentedControl(with captionChoices : [CaptionOptions], segmentedControl : UISegmentedControl){
+        segmentedControl.removeAllSegments()
+        for choice in captionChoices {
+            segmentedControl.insertSegment(withTitle:choice.emoji, at: choice.caption.count, animated: true)
+        }
+        segmentedControl.selectedSegmentIndex = 0
+    }
+    func updateCaption(){
+        topCaptionLabel.text = topChoices[topSegmentedControl.selectedSegmentIndex].caption
+        bottomCaptionLabel.text = bottomChoice[bottomSegmentedControl.selectedSegmentIndex].caption
+    }
+    
+    @IBAction func dragTopLabel(_ sender: UIPanGestureRecognizer) {
+        if sender.state == .changed {
+            topCaptionLabel.center = sender.location(in: view)
+        }
+    }
+    
+    @IBAction func dragBottomLabel(_ sender: UIPanGestureRecognizer) {
+        if sender.state == .changed {
+            bottomCaptionLabel.center = sender.location(in: view)
+           
+        }
+    }
 }
 
